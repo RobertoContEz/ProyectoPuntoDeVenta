39229b96790f8d18c0fdc085c2f031e313afc8bb
RobertoContEz <95501242+RobertoContEz@users.noreply.github.com>
2022-03-17 14:49:39 -0700

    Update Principal.java
    Alv los coments

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -13,9 +13,6 @@ public class Principal {
     public static void main(String[] args) {
         new FmrPrincipal().setVisible(true);
         
-//        IPersistenciaVenta pv = DAOSFactory.crearPersistenciaVenta();
-//        IPersistenciaEntidad<Cliente> pc = DAOSFactory.crearPersistenciaCliente();
-//        IPersistenciaEntidad<Producto> pp = DAOSFactory.crearPersistenciaProducto();
         
     }
     
